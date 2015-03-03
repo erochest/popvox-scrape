25b6a02c70c3a3eb6adeca1ea4fbe8ae90e55d01
Eric Rochester <erochest@virginia.edu>
2015-03-03 13:13:33 -0500

    Refactored command-line parsing into Opts.

--- Main.hs
+++ Main.hs
@@ -26,14 +26,13 @@ import           Data.Text.Format           hiding (print)
 import qualified Data.Text.Format           as F
 import           Data.Traversable
 import qualified Data.Vector                as V
-import           Data.Version
 import           Filesystem
 import           Filesystem.Path.CurrentOS  hiding (concat, decode)
 import           Options.Applicative
 import           Prelude                    hiding (FilePath, mapM)
 import           System.IO                  (hFlush, stdout)
 
-import           Paths_popvox_scrape
+import           Opts
 import           PopVox.Bills
 import           PopVox.Contribs
 import           PopVox.Legislators
@@ -50,6 +49,8 @@ sessions = [109, 110, 111, 112, 113]
 
 main :: IO ()
 main = execParser opts >>= popvox
+
+-- TODO: Break these out into separate modules with smaller functions.
 
 transform :: FilePath -> FilePath -> FilePath -> Script ()
 transform contribDataFile maplightAPIDir outputFile = do
@@ -149,6 +150,7 @@ searchPosition maplightAPIDir maplightOrg = do
     where
         isHit :: BillInfo -> Bool
         isHit = any (maplightOrg `T.isInfixOf`) . getOrg
+
         getOrg bi = bi ^.. billInfoOrgs . traverse . orgInfoName
 
 reportOn :: FilePath -> T.Text -> Script ()
@@ -157,6 +159,7 @@ reportOn reportCsvFile reportTarget =
     >>= V.mapM_ dumpOrg . V.filter isHit . snd
     where
         isHit = T.isInfixOf reportTarget . M.lookupDefault "" "Organization"
+
         dumpOrg :: M.HashMap B.ByteString T.Text -> Script ()
         dumpOrg org = scriptIO $ do
             mapM_ (F.print "\t{} => {}\n" . first Shown)
@@ -174,114 +177,3 @@ popvox TestCsv{..}        = testCSV contribDataFile failFast
 popvox SearchPosition{..} = runScript $ searchPosition maplightAPIDir maplightOrg
 popvox ReportOn{..}       = runScript $ reportOn reportCsvFile reportTarget
 
-transform' :: Parser PopVoxOptions
-transform'
-    =   Transform
-    <$> fileOption (  short 'd' <> long "data-file" <> value "./data.csv"
-                   <> metavar "CONTRIB_DATA_FILE"
-                   <> help "The file containing the contribution\
-                           \ data. Defaults to './data.csv'.")
-    <*> fileOption (  short 'c' <> long "api-dir"
-                   <> metavar "MAPLIGHT_API_DIR"
-                   <> value "./maplight-cache"
-                   <> help "A directory containing API responses.\
-                           \ Defaults to './maplight-cache'.")
-    <*> fileOption (  short 'o' <> long "output" <> metavar "CSV_OUTPUT"
-                   <> help "The file to write the output to.")
-
-rankBills' :: Parser PopVoxOptions
-rankBills'
-    =   RankBills
-    <$> fileOption (  short 's' <> long "score-dir"
-                   <> metavar "VOTEVIEW_DATA_DIR"
-                   <> help "The directory containing the voteview.com\
-                           \ data directory.")
-    <*> fileOption (  short 'b' <> long "bill-dir" <> metavar "BILL_DATA_DIR"
-                   <> help "The directory containing the bill JSON data\
-                           \ from govtrack.us.")
-    <*> fileOption (  short 'l' <> long "legislator-index"
-                   <> metavar "LEGISLATOR_INDEX_DIR"
-                   <> help "The directory containing the legislator index\
-                           \ data from github.com/unitedstates/congress-legislators.")
-    <*> fileOption (  short 'o' <> long "output" <> metavar "CSV_OUTPUT"
-                   <> help "The file to write the output to.")
-
-testJson' :: Parser PopVoxOptions
-testJson'
-    =   TestJson
-    <$> fileOption (  short 'c' <> long "api-dir"
-                   <> metavar "MAPLIGHT_API_DIR"
-                   <> value "./maplight-cache"
-                   <> help "A directory containing API responses.\
-                           \ Defaults to './maplight-cache'.")
-
-testCsv' :: Parser PopVoxOptions
-testCsv'
-    =   TestCsv
-    <$> fileOption (  short 'd' <> long "data-file" <> value "./data.csv"
-                   <> metavar "CONTRIB_DATA_FILE"
-                   <> help "The directory containing the contribution\
-                           \ data. Defaults to './data.csv'.")
-    <*> switch     (  short 'f' <> long "fail-fast"
-                   <> help "Short-circuit processing on the first error.")
-
-searchPosition' :: Parser PopVoxOptions
-searchPosition'
-    =   SearchPosition
-    <$> fileOption (  short 'c' <> long "api-dir"
-                   <> metavar "MAPLIGHT_API_DIR"
-                   <> value "./maplight-cache"
-                   <> help "A directory containing API responses.\
-                           \ Defaults to './maplight-cache'.")
-    <*> textArg    (  metavar "SEARCH_TARGET"
-                   <> help "The name of the organization to search for.")
-
-reportOn' :: Parser PopVoxOptions
-reportOn'
-    =   ReportOn
-    <$> fileOption (  short 'c' <> long "csv-file"
-                   <> metavar "CSV_OUTPUT_FILE"
-                   <> value "./maplight-data.csv"
-                   <> help "An output file to look for information\
-                           \ about an organization.")
-    <*> textArg    (  metavar "SEARCH_TARGET"
-                   <> help "The name of the organization to search for.")
-
-opts' :: Parser PopVoxOptions
-opts' = subparser
-      ( command "transform"
-        (info (helper <*> transform')
-            (progDesc "Read the data files, transform and join, and output."))
-      <> command "rank-bills"
-         (info (helper <*> rankBills')
-            (progDesc "Determine the bias of each bill from the sponsor's scores."))
-      <> command "test-json"
-         (info (helper <*> testJson')
-            (progDesc "Read the JSON API files and test that we can parse them."))
-      <> command "test-csv"
-         (info (helper <*> testCsv')
-            (progDesc "Read the CSV data files and test that we can parse them."))
-      <> command "search-pos"
-         (info (helper <*> searchPosition')
-            (progDesc "Search the bill information organizations."))
-      <> command "report-on"
-         (info (helper <*> reportOn')
-            (progDesc "Report on the information in the data file for\
-                      \ an organization."))
-      )
-
-opts :: ParserInfo PopVoxOptions
-opts = info (helper <*> opts')
-            (  fullDesc
-            <> progDesc "Assemble a dataset of bill positions and\
-                        \ political contributions."
-            <> header ("popvox-scrape v" ++ showVersion version))
-
-textOpt :: Mod OptionFields T.Text -> Parser T.Text
-textOpt = option (T.pack <$> str)
-
-fileOption :: Mod OptionFields FilePath -> Parser FilePath
-fileOption = option (decodeString <$> str)
-
-textArg :: Mod ArgumentFields T.Text -> Parser T.Text
-textArg = argument (T.pack <$> str)
--- Opts.hs
+++ Opts.hs
@@ -7,51 +7,121 @@ module Opts
     ) where
 
 
-import qualified Data.ByteString.Char8     as BS
 import qualified Data.Text                 as T
 import           Data.Version
 import           Filesystem.Path.CurrentOS
 import           Options.Applicative
-import           Options.Applicative.Types
 import           Prelude                   hiding (FilePath)
 
 import           Paths_popvox_scrape
 import           PopVox.Types
 
 
+transform' :: Parser PopVoxOptions
+transform'
+    =   Transform
+    <$> fileOption (  short 'd' <> long "data-file" <> value "./data.csv"
+                   <> metavar "CONTRIB_DATA_FILE"
+                   <> help "The file containing the contribution\
+                           \ data. Defaults to './data.csv'.")
+    <*> fileOption (  short 'c' <> long "api-dir"
+                   <> metavar "MAPLIGHT_API_DIR"
+                   <> value "./maplight-cache"
+                   <> help "A directory containing API responses.\
+                           \ Defaults to './maplight-cache'.")
+    <*> fileOption (  short 'o' <> long "output" <> metavar "CSV_OUTPUT"
+                   <> help "The file to write the output to.")
+
+rankBills' :: Parser PopVoxOptions
+rankBills'
+    =   RankBills
+    <$> fileOption (  short 's' <> long "score-dir"
+                   <> metavar "VOTEVIEW_DATA_DIR"
+                   <> help "The directory containing the voteview.com\
+                           \ data directory.")
+    <*> fileOption (  short 'b' <> long "bill-dir" <> metavar "BILL_DATA_DIR"
+                   <> help "The directory containing the bill JSON data\
+                           \ from govtrack.us.")
+    <*> fileOption (  short 'l' <> long "legislator-index"
+                   <> metavar "LEGISLATOR_INDEX_DIR"
+                   <> help "The directory containing the legislator index\
+                           \ data from github.com/unitedstates/congress-legislators.")
+    <*> fileOption (  short 'o' <> long "output" <> metavar "CSV_OUTPUT"
+                   <> help "The file to write the output to.")
+
+testJson' :: Parser PopVoxOptions
+testJson'
+    =   TestJson
+    <$> fileOption (  short 'c' <> long "api-dir"
+                   <> metavar "MAPLIGHT_API_DIR"
+                   <> value "./maplight-cache"
+                   <> help "A directory containing API responses.\
+                           \ Defaults to './maplight-cache'.")
+
+testCsv' :: Parser PopVoxOptions
+testCsv'
+    =   TestCsv
+    <$> fileOption (  short 'd' <> long "data-file" <> value "./data.csv"
+                   <> metavar "CONTRIB_DATA_FILE"
+                   <> help "The directory containing the contribution\
+                           \ data. Defaults to './data.csv'.")
+    <*> switch     (  short 'f' <> long "fail-fast"
+                   <> help "Short-circuit processing on the first error.")
+
+searchPosition' :: Parser PopVoxOptions
+searchPosition'
+    =   SearchPosition
+    <$> fileOption (  short 'c' <> long "api-dir"
+                   <> metavar "MAPLIGHT_API_DIR"
+                   <> value "./maplight-cache"
+                   <> help "A directory containing API responses.\
+                           \ Defaults to './maplight-cache'.")
+    <*> textArg    (  metavar "SEARCH_TARGET"
+                   <> help "The name of the organization to search for.")
+
+reportOn' :: Parser PopVoxOptions
+reportOn'
+    =   ReportOn
+    <$> fileOption (  short 'c' <> long "csv-file"
+                   <> metavar "CSV_OUTPUT_FILE"
+                   <> value "./maplight-data.csv"
+                   <> help "An output file to look for information\
+                           \ about an organization.")
+    <*> textArg    (  metavar "SEARCH_TARGET"
+                   <> help "The name of the organization to search for.")
+
 opts' :: Parser PopVoxOptions
-opts' =   PopVoxOptions
-      <$> fileOpt (  short 's'
-                  <> long "opensecrets"
-                  <> metavar "DIRNAME"
-                  <> help "The location of the opensecrets.org data sets.")
-      <*> fileOpt (  short 'o'
-                  <> long "output"
-                  <> metavar "OUTPUT_FILE"
-                  <> help "The CSV filename to write the data to.")
-      <*> apiOpt  (  short 'a'
-                  <> long "api"
-                  <> metavar "POPVOX_API"
-                  <> help "You PopVox API key.")
-      <*> switch  (  short 'v'
-                  <> long "verbose"
-                  <> help "Print extra information.")
+opts' = subparser
+      ( command "transform"
+        (info (helper <*> transform')
+            (progDesc "Read the data files, transform and join, and output."))
+      <> command "rank-bills"
+         (info (helper <*> rankBills')
+            (progDesc "Determine the bias of each bill from the sponsor's scores."))
+      <> command "test-json"
+         (info (helper <*> testJson')
+            (progDesc "Read the JSON API files and test that we can parse them."))
+      <> command "test-csv"
+         (info (helper <*> testCsv')
+            (progDesc "Read the CSV data files and test that we can parse them."))
+      <> command "search-pos"
+         (info (helper <*> searchPosition')
+            (progDesc "Search the bill information organizations."))
+      <> command "report-on"
+         (info (helper <*> reportOn')
+            (progDesc "Report on the information in the data file for\
+                      \ an organization."))
+      )
 
 opts :: ParserInfo PopVoxOptions
 opts = info (helper <*> opts')
             (  fullDesc
-            <> progDesc "Scrapes data from popvox.com and \
-                        \mashes it with opensecrets.org data."
+            <> progDesc "Assemble a dataset of bill positions and\
+                        \ political contributions."
             <> header ("popvox-scrape v" ++ showVersion version))
 
-fileOpt :: Mod OptionFields FilePath -> Parser FilePath
-fileOpt = option (decodeString <$> readerAsk)
+fileOption :: Mod OptionFields FilePath -> Parser FilePath
+fileOption = option (decodeString <$> str)
 
-textOpt :: Mod OptionFields T.Text -> Parser T.Text
-textOpt = option (T.pack <$> readerAsk)
-
-byteOpt :: Mod OptionFields BS.ByteString -> Parser BS.ByteString
-byteOpt = option (BS.pack <$> readerAsk)
-
-apiOpt :: Mod OptionFields APIKey -> Parser APIKey
-apiOpt = option (APIKey . T.pack <$> readerAsk)
+textArg :: Mod ArgumentFields T.Text -> Parser T.Text
+textArg = argument (T.pack <$> str)
